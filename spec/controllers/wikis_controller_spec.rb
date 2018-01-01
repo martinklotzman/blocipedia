c8b96c0822e224b8b4e9253a63181c61bd160efa
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2018-01-01 10:36:45 -0800

    wiki controller GET index

--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -1,38 +1,44 @@
 require 'rails_helper'
+include RandomData
 
 RSpec.describe WikisController, type: :controller do
-let(:my_wiki) { Wiki.create!(title: "New Wiki Title", body: "New Wiki Body") }
-let(:my_user) { User.create!(email: "test@gmail.com", password: "password", password_confirmation: "password") }
+let(:my_wiki) { Wiki.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+let(:my_user) { User.create!(email: RandomData.random_email, password: "password", password_confirmation: "password") }
 
   describe "GET #index" do
-
 
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
-  end
 
-  describe "GET #show" do
-
-    it "returns http success" do
-      get :show, params: { id: my_wiki.id }
-      expect(response).to have_http_status(:success)
+    it "assigns [my_wiki] to @wikis" do
+      get :index
+      expect(assigns(:wikis)).to eq([my_wiki])
     end
+
   end
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET #show" do
+  #
+  #   it "returns http success" do
+  #     get :show, params: { id: my_wiki.id }
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+  # describe "GET #new" do
+  #   it "returns http success" do
+  #     get :new
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+  # describe "GET #edit" do
+  #   it "returns http success" do
+  #     get :edit
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
