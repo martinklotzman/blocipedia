5153d210ffa5a9fc3391d9a29ab3f8d8aecb2493
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2018-01-01 16:35:39 -0800

    wiki_controllers new create tests not passing

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -7,6 +7,21 @@ class WikisController < ApplicationController
   end
 
   def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+
+    if @wiki.save
+      flash[:notice] = "Wiki was saved successfully."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "There was an error saving the wiki. Please try again."
+      render :new
+    end
   end
 
   def edit
--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -27,12 +27,38 @@ let(:my_user) { User.create!(email: RandomData.random_email, password: "password
   #   end
   # end
   #
-  # describe "GET #new" do
-  #   it "returns http success" do
-  #     get :new
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+    it "instantiates @wiki" do
+      get :new
+      expect(assigns(:wiki)).not_to be_nil
+    end
+  end
+
+  describe "WIKI create" do
+    it "increases the number of Wiki by 1" do
+      expect{ post :create, params: { wiki: { title: RandomData.random_sentence, body: RandomData.random_paragraph }}}.to change(Wiki,:count).by(1)
+    end
+
+    it "assigns the new wiki to @wiki" do
+      post :create, params: { wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph }}
+      expect(assigns(:wiki)).to eq Wiki.last
+    end
+
+    it "redirects to the new wiki" do
+      post :create, params: {wiki: { title: RandomData.random_sentence, body: RandomData.random_paragraph }}
+      expect(response).to redirect_to Wiki.last
+    end
+  end
   #
   # describe "GET #edit" do
   #   it "returns http success" do
