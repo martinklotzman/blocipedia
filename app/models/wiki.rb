50d7dc43d65edb02b76dd60b9f4fa93f0104bb0d
Martin Klotzman <martinklotzman@Martins-MacBook-Air.local>
2018-02-07 18:45:58 -0800

    fix bug in wiki_policy. now collaborted wikis appear for standard users

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,7 +1,7 @@
 class Wiki < ApplicationRecord
   belongs_to :user, optional: true
-  has_many :users, through: :collaborators
   has_many :collaborators, dependent: :destroy
+  has_many :users, through: :collaborators
 
   def make_public!
     update_attribute(:private, false)
